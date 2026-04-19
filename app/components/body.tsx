938f7948c7acf7561ba2c06441e032c91584f9dc
Yeminfu <yeminfu4319@gmail.com>
2026-04-19 22:14:57 +1000

    сделал кнопку вызова формы

--- app/components/body.tsx
+++ app/components/body.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
+import LogoImg from "@/app/images/logo.png";
 
 export default function Body(): React.ReactNode {
   return (
     <>
-     
+      <main>
+        <div className="max-w-4xl mx-auto px-4 container">
+          <div className="mt-4">
+    <button className="rounded-full p-1 cursor-pointer w-full hover:bg-gray-300 bg-white shadow-2xl shadow-black/40 transition-shadow duration-200 hover:shadow-black/50">              <div className="p-2 relative">
+                <Image
+                  src={LogoImg}
+                  alt="Background"
+                  width={0}
+                  height={0}
+                  sizes="100vh"
+                  className="absolute left-0 top-0 z-10 h-full w-auto" // ← -z-10
+                  priority
+                />
+                <div className="text-center relative z-10">
+                  {" "}
+                  {/* ← z-10 для текста */}
+                  <div>СТАТЬ ДОБРОВОЛЬЦЕМ</div>
+                  <div>стать частью команды</div>
+                </div>
+              </div>
+            </button>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
