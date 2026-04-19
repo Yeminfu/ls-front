e0af03087c4209f4f75e93cefc4af93c59f22ee3
Yeminfu <yeminfu4319@gmail.com>
2026-04-19 21:15:05 +1000

    сделал шапку

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,12 +1,50 @@
-// import Link from "next/link";
-// import LogoImg from "@/app/images/logo.jpg"
-// import Image from "next/image";
+import LogoImg from "@/app/images/logo.png";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <>
       <header className="header" role="banner">
-        
+        <div className="flex items-center justify-center ">
+          <Image
+            src={LogoImg}
+            alt=""
+            width={95}
+            height={95}
+            className="rounded-full mt-5"
+            placeholder="blur"
+          />
+        </div>
+        <div className="mt-5">
+          <div className="flex items-center justify-center ">
+            <h1 className="text-1xl font-bold">
+              ДОБРОВОЛЬЧЕСКИЙ ПОИСКОВО-СПАСАТЕЛЬНЫЙ ОТРЯД ‌ «ЛИГА СПАС»
+            </h1>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <div className="max-w-4xl mx-auto px-4 container">
+            Отряд существует с 2016 года, действует на территории Хабаровского
+            края и Еврейской автономной области, является некоммерческой
+            организацией и одним из сильнейших поисковых отрядов нашей страны.
+            Отряд служит дополнительным ресурсом в поиске пропавших людей для
+            УМВД, СУ СКР и МЧС России по региону. С 2018 года является
+            региональным отделением Национального центра помощи.
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <div className="max-w-4xl mx-auto px-4 container">
+            <div className="flex items-center justify-center ">
+              <div>
+                <span style={{ color: "#fd7e14" }} className="text-2xl">
+                  ТЫ НУЖЕН ЛЮДЯМ, НУЖЕН ДАЛЬНЕМУ ВОСТОКУ!
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </>
   );
