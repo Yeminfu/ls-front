53ee72fbb2dce303068f9ae15e531fb10f79cc5f
Yeminfu <yeminfu4319@gmail.com>
2026-04-19 19:54:28 +1000

    up

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,16 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer" role="contentinfo">
-      <div className="container">
-        <div className="footer__text">  {/* меняем p на div */}
-          <div>
-            © 2016–2026 Добровольческий поисково-спасательный отряд «Лига Спас»
-          </div>
-          <div>
-            Региональное отделение Национального центра помощи пропавшим и пострадавшим людям.
-          </div>
-        </div>
-      </div>
+     
     </footer>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import Body from "./components/body";
 import Footer from "./components/footer";
 import Header from "./components/header/header";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "v", // Путь к файлу в папке public
+    apple: "./images/icon.jpg",
+    // ... другие иконки
+  },
+};
 
 export default function Home() {
   return (
