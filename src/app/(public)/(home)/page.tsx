c16ee506066290b45f1688bb008d552e7314b7c9
Yeminfu <yeminfu4319@gmail.com>
2026-05-30 01:00:49 +1000

    вывел компонент для команды

--- src/app/(public)/(home)/page.tsx
+++ src/app/(public)/(home)/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Cause } from "./components/cause";
 import { CAUSES } from "./data/causes.data";
+import { TEAM } from "./data/team.data";
 
 export const metadata: Metadata = {
   title: "Лига Спас — Добровольческий поисково-спасательный отряд",
@@ -26,6 +27,35 @@ export default function Home() {
           {CAUSES.map((cause, i) => (
             <Cause key={i} cause={cause} />
           ))}
+        </div>
+      </section>
+
+      <section>
+        <div className="mt-4">
+          <div className="max-w-5xl mx-auto px-4 container">
+            <div className="text-center text-2xl font-bold">НАША КОМАНДА</div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
+              {
+                TEAM.map((teammate, i) => {
+                  return (
+                    <div key={i} className="rounded-lg p-4">
+                      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                        {(() => {
+                          return <img src={teammate.image} />;
+                        })()}
+                        <div className="px-6 py-4">
+                          <div className="font-bold text-m mb-2">
+                            {teammate.title}
+                          </div>
+                          <p>{teammate.description}</p>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })
+              }
+            </div>
+          </div>
         </div>
       </section>
 
---
+++ src/app/(public)/(home)/types/team.interface.ts
@@ -0,0 +1,5 @@
+export interface ts_teammate {
+  image: string;
+  title: string;
+  description: string;
+}
